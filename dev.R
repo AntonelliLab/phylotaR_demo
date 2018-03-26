1c759bd9793bfe2d0c97e1e69ff4972733317736
Dom Bennett <dominic.john.bennett@gmail.com>
2018-03-26 14:56:58 +0200

    Update

--- dev.R
+++ dev.R
@@ -1,36 +1,34 @@
-  mkdata <- function(cid) {
-    cl <- phylota@cls@cls[[which(cid == phylota@cids)]]
-    value <- apply(X=txids_sqs[ ,cl@sids], 1, any)
-    value <- factor(as.numeric(value))
-    data.frame(txid=as.character(txids),
-               cid=cid, value=value)
-  }
-  txidinsqs <- function(txid) {
-    vapply(phylota@sids, function(x) {
-      is_txid_in_sq(phylota=phylota, txid=txid, sid=x)
-      }, logical(1))
-  }
-  txids_sqs <- lapply(txids, txidinsqs)
-  txids_sqs <- do.call('rbind', txids_sqs)
-  value <- NULL
-  # gen p_data
-  p_data <- lapply(cids, mkdata)
-  p_data <- do.call('rbind', p_data)
-  p_data[['cnm']] <- 
-    cnms[match(p_data[['cid']], cids)]
-  p_data[['txnm']] <- 
-    txnms[match(p_data[['txid']], txids)]
-  # reorder
-  p_data[['cnm']] <- factor(p_data[['cnm']],
-                            levels=cord,
-                            ordered=TRUE)
-  p_data[['txnm']] <- factor(p_data[['txnm']],
-                             levels=txord,
-                             ordered=TRUE)
-  cnm <- txnm <- NULL
-  ggplot2::ggplot(p_data, ggplot2::aes(cnm, txnm)) +
-    ggplot2::geom_tile(ggplot2::aes(fill=value)) +
-    ggplot2::xlab('') + ggplot2::ylab('') +
-    ggplot2::scale_fill_manual(values=c('white', 'black')) +
-    ggplot2::theme_bw() +
-    ggplot2::theme(legend.position='none')
+mkdata <- function(cid) {
+  cl <- phylota@cls@cls[[which(cid == phylota@cids)]]
+  value <- apply(X=tis_mtrx[ ,cl@sids], 1, any)
+  value <- as.numeric(value)
+  data.frame(txid=as.character(txids),
+             cid=cid, value=value)
+}
+value <- NULL
+# gen p_data
+tis_mtrx <- phylotaR:::mk_txid_in_sq_mtrx(phylota=phylota,
+                               txids=txids)
+p_data <- lapply(cids, mkdata)
+p_data <- do.call('rbind', p_data)
+p_data[['cnm']] <- 
+  cnms[match(p_data[['cid']], cids)]
+p_data[['txnm']] <- 
+  txnms[match(p_data[['txid']], txids)]
+# reorder
+p_data[['cnm']] <- factor(p_data[['cnm']],
+                          levels=cnms,
+                          ordered=TRUE)
+p_data[['txnm']] <- factor(p_data[['txnm']],
+                           levels=txnms,
+                           ordered=TRUE)
+# plot
+# https://stackoverflow.com/questions/9439256/how-can-i-handle-r-cmd-check-no-visible-binding-for-global-variable-notes-when
+cnm <- txnm <- NULL
+ggplot2::ggplot(p_data, ggplot2::aes(cnm, txnm)) +
+  ggplot2::geom_tile(ggplot2::aes(fill=value)) +
+  ggplot2::xlab('') + ggplot2::ylab('') +
+  ggplot2::scale_fill_gradient(low='#e0e0e0',
+                               high='#303030') +
+  ggplot2::theme_bw() +
+  ggplot2::theme(legend.position='none')
